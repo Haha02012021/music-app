8c66a209a83d669ea7f6c992fb887de6b93667c3
hangdt-0262 <dao.thu.hang@sun-asterisk.com>
2023-11-22 12:06:49 +0700

    update auth service

--- server/app/Http/Controllers/AuthenticateController.php
+++ server/app/Http/Controllers/AuthenticateController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CustomRequest;
+use App\Models\AccessToken;
 use App\Services\AuthenticateService;
 use Illuminate\Http\Request;
 
@@ -67,6 +69,38 @@ class AuthenticateController extends Controller
             'success' => true,
             'data' => $account,
             'message' => 'Lấy thông tin người dùng thành công!',
+        ]);
+    }
+
+    public function logout(CustomRequest $request)
+    {
+        $accessToken = $request->bearerToken();
+        $appId = env('ZALO_CLIENT_ID');
+        $payload = [
+            'access_token' => $accessToken,
+            'app_id' => $appId,
+        ];
+        $logoutCurl = curl_init();
+        $logoutUrl = 'https://id.zalo.me/account/logout?continue='.env('APP_URL');
+        $logoutHeader = array(
+            'Content-Type:application/x-www-form-urlencoded',
+            'secret_key:'.env('ZALO_CLIENT_SECRET')
+        );
+        curl_setopt_array($logoutCurl, array(
+            CURLOPT_URL => $logoutUrl,
+            CURLOPT_RETURNTRANSFER => true, 
+            CURLOPT_TIMEOUT => 3600,   
+            CURLOPT_POST => true,   
+            CURLOPT_POSTFIELDS => http_build_query($payload) 
+        ));
+        curl_setopt($logoutCurl, CURLOPT_HTTPHEADER, $logoutHeader);
+        $logoutRes = curl_exec($logoutCurl);
+        $logoutR = json_decode($logoutRes);
+        curl_close($logoutCurl);
+
+        return response()->json([
+            'success' => true,
+            'data' => $logoutR
         ]);
     }
 }

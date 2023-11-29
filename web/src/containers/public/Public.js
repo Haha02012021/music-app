722354eb29b79a409c9c1a277130f531c98e3c3c
hangdt-0262 <dao.thu.hang@sun-asterisk.com>
2023-11-29 09:24:31 +0700

    fix open right side bar

--- web/src/containers/public/Public.js
+++ web/src/containers/public/Public.js
@@ -9,9 +9,13 @@ const Public = () => {
   const [openRightSideBar, setOpenRightSideBar] = useState(false);
   const { curSongId } = useSelector(state => state.music);
 
+  const handleOpenSidebar = () => {
+    setOpenRightSideBar(!openRightSideBar)
+  }
+
   return (
     <div className='w-full h-screen relative flex flex-col bg-main-300'>
-      <div className='w-full flex flex-auto overflow-hidden'>
+      <div className='w-full flex flex-auto overflow-hidden relative'>
         <div className='h-full w-[240px] flex-none'>
           <LeftSideBar />
         </div>
@@ -24,14 +28,14 @@ const Public = () => {
         </div>
 
         { openRightSideBar && 
-          <div className='w-[329px] hidden 1600:flex flex-none animate-slide-left'>
+          <div className='w-[329px] absolute top-0 right-0 bg-white border-l-2 1600:flex flex-none animate-slide-left'>
             <RightSideBar />
           </div>
         }
       </div>
       
       {curSongId && <div className='fixed bottom-0 left-0 right-0 h-[90px]'>
-        <Player setOpenRightSideBar={setOpenRightSideBar} openRightSideBar={openRightSideBar}/>
+        <Player setOpenRightSideBar={handleOpenSidebar} openRightSideBar={openRightSideBar}/>
       </div>}
     </div>
   )

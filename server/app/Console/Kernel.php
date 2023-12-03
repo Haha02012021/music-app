bf722d2c5e68fa1e27394a10f3f2ce4dab11d9a3
hangdt-0262 <dao.thu.hang@sun-asterisk.com>
2023-12-04 02:08:42 +0700

    update docker-compose laravel

--- server/app/Console/Kernel.php
+++ server/app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('app:top100-command')->weeklyOn(1, '02:00');
+        $schedule->command('app:top100-command')->weeklyOn(1, '02:15');
     }
 
     /**

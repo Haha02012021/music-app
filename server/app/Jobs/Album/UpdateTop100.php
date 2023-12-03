39d314dfe884ab34ea5f4b1a7f9939f0f93ec47c
hangdt-0262 <dao.thu.hang@sun-asterisk.com>
2023-12-04 01:23:44 +0700

    update handle job update top 100

--- server/app/Jobs/Album/UpdateTop100.php
+++ server/app/Jobs/Album/UpdateTop100.php
@@ -2,12 +2,16 @@
 
 namespace App\Jobs\Album;
 
+use App\Services\AlbumService;
+use App\Services\FileService;
+use Google\Cloud\Storage\StorageClient;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Kreait\Firebase\Storage;
 
 class UpdateTop100 implements ShouldQueue
 {
@@ -26,6 +30,27 @@ class UpdateTop100 implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $storageClient = new StorageClient();
+        $storage = new Storage($storageClient);
+        $fileService = new FileService($storage);
+        $albumService = new AlbumService($fileService);
+        $vietnam = 'Việt Nam';
+        $usuk = 'Âu Mỹ';
+
+        $vietnamSongs = $albumService->getSongsByGenreName($vietnam);
+        $usukSongs = $albumService->getSongsByGenreName($usuk);
+        $asiaSongs = $albumService->getSongsOfAsia(); 
+
+        $asiaSongs->map(function ($item) use ($albumService) {
+            return $albumService->updateTop100($item, 1);
+        });
+
+        $vietnamSongs->map(function ($item) use ($albumService) {
+            return $albumService->updateTop100($item, 1);
+        });
+
+        $usukSongs->map(function ($item) use ($albumService) {
+            return $albumService->updateTop100($item, 1);
+        });
     }
 }
